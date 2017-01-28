23a446f08d93c7daeeaa722ac4ca7c3ec4e1ef71
John Eriksson <john@provideit.se>
2017-01-28 13:09:22 +0100

    Updated app to use new versions of express + socket.io

--- index.js
+++ index.js
@@ -2,53 +2,73 @@ process.chdir(__dirname);
 require('dotenv').config();
 
 // Load dependencies
-const express = require('express.io');
+const express = require('express');
+const Server = require('http').Server;
 const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 const models = require('./models');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const redis = require('redis');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const passportIO = require('passport.socketio');
+const passportUtils = require('./lib/passport-utils');
+const _ = require('lodash');
 
 mongoose.Promise = require('promise');
 // Connect to mongo
 mongoose.connect(process.env.MONGODB_URL);
 
 // Setup session store
-const sessionStore = new RedisStore({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
-  client: redis.createClient()
+const sessionStore = session({
+  secret: process.env.SESSION_SECRET || "Veronica!",
+  resave: false,
+  saveUninitialized: false,
+  store: new RedisStore({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
+    client: redis.createClient()
+  })
 });
 
 // Setup app
-const app = express().http().io();
+const app = express();
+const server = Server(app);
+const io = require('socket.io')(server);
+
+// Configure app
 app.use(express.static(path.join(__dirname, 'public/assets')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(session({
-  secret: process.env.SESSION_SECRET || "Veronica!",
-  resave: false,
-  saveUninitialized: false,
-  store: sessionStore
-}));
-app.io.set('authorization', passportIO.authorize({
-  cookieParser: cookieParser,
-  key: 'connect.sid',
-  secret: process.env.SESSION_SECRET || "Veronica!",
-  store: sessionStore
-}));
 
+// Session setup
+io.use((socket, next) => {
+  sessionStore(socket.request, socket.request.res, next);
+});
+io.use((socket, next) => {
+  let uid = _.get(socket, 'request.session.passport.user');
+  if (uid) {
+    passportUtils.deserializeUser(uid, (err, user) => {
+      if (err) return next(err);
+      if (!user) return next(new Error('Invalid user'));
+
+      socket.request.user = user;
+      next();
+    });
+  } else {
+    next();
+  }
+});
+app.use(sessionStore);
+
+// View engine setup
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'public/views'));
 
+// HTTP logging
 if (process.env.HTTP_LOG_FILE) {
   try {
     const stats = fs.statSync(process.env.HTTP_LOG_FILE);
@@ -66,40 +86,7 @@ if (process.env.HTTP_LOG_FILE) {
 }
 
 // User authentication
-passport.use(new LocalStrategy((username, password, done) => {
-  models.User.findOne({
-    $or: [
-      {username: username},
-      {email: username}
-    ]
-  }, (err, user) => {
-    if (err) return done(err);
-    if (!user) return done(null, false);
-
-    user.verifyPassword(password, (err, res) => {
-      if (err) return done(err);
-      if (!res) return done(null, false);
-
-      return done(null, user);
-    });
-  });
-}));
-
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((uid, done) => {
-  models.User.findOne({
-    _id: uid
-  }, (err, user) => {
-    if (err) return done(err, false);
-    if (!user) return done(null, false);
-
-    return done(null, user);
-  });
-});
-
+passportUtils.setup(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -107,6 +94,10 @@ app.use(passport.session());
 fs.readdirSync(path.join(__dirname, 'routes')).forEach(r => {
   // Only handle .js files
   if (r.substr(-3) !== '.js') {
+    return;
+  }
+
+  if (r.indexOf('socketroutes') !== -1) {
     return;
   }
 
@@ -119,10 +110,14 @@ fs.readdirSync(path.join(__dirname, 'routes')).forEach(r => {
   route(app); // Initialize route
 });
 
-setTimeout(() => {
+// Setup websockets
+io.on('connection', (socket) => {
+  require('./routes/socketroutes')(io, socket);
+});
+
+// setTimeout(() => {
   // Start application
-  app.listen(process.env.NODE_PORT, () => {
+  server.listen(process.env.NODE_PORT, () => {
     console.log(`App running on port ${process.env.NODE_PORT}`);
   });
-}, 5000);
-
+// }, 5000);
---
+++ routes/socketroutes.js
@@ -0,0 +1,189 @@
+const models = require('../models');
+const statistics = require('../lib/statistics');
+const async = require('async');
+
+module.exports = (io, socket) => {
+  socket.on('liquid:list', (respond) => {
+    models.Liquid.find({
+      author: socket.request.user._id
+    }).populate('flavours.flavour').populate('comments').exec((err, liquids) => {
+      if (err) {
+        return respond({
+          error: 'Failed to fetch liquids'
+        });
+      }
+
+      return respond(liquids);
+    });
+  });
+
+  socket.on('liquid:create', (data, respond) => {
+    const liquid = new models.Liquid({
+      name: data.name,
+      base: {
+        nicStrength: (data.base || {}).nicStrength
+      },
+      target: {
+        pgPercent: (data.target || {}).pgPercent,
+        vgPercent: (data.target || {}).vgPercent,
+        nicStrength: (data.target || {}).nicStrength
+      },
+      flavours: (data.flavours || []).filter(f => { return !!f }).map(f => {
+        if (typeof f.flavour == 'object') {
+          f.flavour = f.flavour._id
+        }
+
+        return f;
+      }),
+      author: socket.request.user._id
+    });
+
+    liquid.save(err => {
+      if (err) {
+        return respond({
+          error: err
+        });
+      }
+
+      statistics.calculateFlavourAverage((err, results) => {
+        if (err) return false;
+
+        async.each(results, (item, callback) => {
+          models.Flavour.findOne({
+            _id: item._id
+          }, (err, flavour) => {
+            if (err) return callback(err);
+            if (!flavour) return;
+
+            flavour.basePercent = Math.round(item.value.avg * 100) / 100 || 0;
+
+            flavour.save(err => {
+              if (err) return callback(err);
+
+              io.emit('flavour:updated', flavour);
+              return callback(null);
+            });
+          });
+        }, err => {
+          if (err) {
+            console.error(new Error(err));
+          }
+        });
+      });
+
+      models.Liquid.populate(liquid, {
+        path: 'flavours.flavour'
+      }, (err, liquid) => {
+        if (err) return;
+        socket.emit('liquid:created', liquid);
+        respond(liquid);
+      });
+    });
+  });
+
+  socket.on('liquid:update', (data, respond) => {
+    if (!data._id) {
+      return respond({
+        error: "Missing _id"
+      });
+    }
+
+    models.Liquid.findOne({
+      _id: data._id
+    }, (err, liquid) => {
+      if (err) return respond({error: err});
+      if (!liquid) return respond({error: 'Invalid liquid'});
+
+      liquid = Object.assign(liquid, {
+        name: data.name,
+        base: {
+          nicStrength: (data.base || {}).nicStrength
+        },
+        target: {
+          pgPercent: (data.target || {}).pgPercent,
+          vgPercent: (data.target || {}).vgPercent,
+          nicStrength: (data.target || {}).nicStrength
+        },
+        flavours: (data.flavours || []).filter(f => { return !!f && f.perc > 0; }).map(f => {
+          if (typeof f.flavour == 'object') {
+            f.flavour = f.flavour._id
+          }
+
+          return f;
+        }),
+      });
+
+      liquid.save(err => {
+        if (err) {
+          return respond({
+            error: err
+          });
+        }
+
+        statistics.calculateFlavourAverage((err, results) => {
+          if (err) return false;
+
+          async.each(results, (item, callback) => {
+            models.Flavour.findOne({
+              _id: item._id
+            }, (err, flavour) => {
+              if (err) return callback(err);
+              if (!flavour) return;
+
+              flavour.basePercent = (Math.round(item.value.avg * 100) / 100) || 0;
+
+              flavour.save(err => {
+                if (err) return callback(err);
+
+                io.emit('flavour:updated', flavour);
+                return callback(null);
+              });
+            });
+          }, err => {
+            if (err) console.error(new Error(err));
+          });
+        });
+
+        models.Liquid.populate(liquid, {
+          path: 'flavours.flavour'
+        }, (err, liquid) => {
+          if (err) return;
+          socket.emit('liquid:updated', liquid);
+          respond(liquid);
+        });
+      });
+    });
+  });
+
+  socket.on('flavour:list', (respond) => {
+    models.Flavour.find({}).exec((err, flavours) => {
+      if (err) {
+        return respond({
+          error: 'Failed to fetch flavours'
+        });
+      }
+
+      return respond(flavours);
+    });
+  });
+
+  socket.on('flavour:create', (data, respond) => {
+    const flavour = new models.Flavour({
+      name: data.name,
+      basePercent: 0,
+      isVg: data.isVg || false,
+      addedBy: socket.request.user._id
+    });
+
+    flavour.save(err => {
+      if (err) {
+        return respond({
+          error: err
+        });
+      }
+
+      io.emit('flavour:created', flavour);
+      return respond(flavour);
+    });
+  });
+};
